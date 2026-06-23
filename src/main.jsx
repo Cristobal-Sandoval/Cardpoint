556742e0835f751bf51700273e7c849a16540375
crsandovalb <cristobal.sandoval.balboa@gmail.com>
2026-06-23 16:34:37 -0400

    chore: Change admin route to secret path

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="/*" element={<App />} />
-        <Route path="/admin/*" element={<AdminApp />} />
+        <Route path="/acceso-privado-cp/*" element={<AdminApp />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
